4925bf7c819886210f44e2851e182eeb412c7522
下新原佑哉 <>
2020-01-17 22:29:32 +0900

    インポート、関数の追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests\CreateTask;
 
 use App\Folder;
 use App\Task;
@@ -30,4 +31,18 @@ class TaskController extends Controller
             'folder_id' => $id
         ]);
     }
+    public function create(int $id, CreateTask $request)
+{
+    $current_folder = Folder::find($id);
+
+    $task = new Task();
+    $task->title = $request->title;
+    $task->due_date = $request->due_date;
+
+    $current_folder->tasks()->save($task);
+
+    return redirect()->route('tasks.index', [
+        'id' => $current_folder->id,
+    ]);
+}
 }
