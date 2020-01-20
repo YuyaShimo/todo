17b047ac586d877d5e3073196eeef912cb71dadf
下新原佑哉 <>
2020-01-20 19:46:30 +0900

    認証機能の追加

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@
 |
 */
 
-
+Route:group(['middleware' => 'auth'], function(){
+  Route::get('/', 'HomeController@index')->name('home');
+})
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
