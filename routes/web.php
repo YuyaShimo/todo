63634126c1efdeb3d18ab6c9160350367c34cac5
下新原佑哉 <>
2020-01-21 01:19:49 +0900

    ルーティングの訂正

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route:group(['middleware' => 'auth'], function(){
+Route::group(['middleware' => 'auth'], function(){
   Route::get('/', 'HomeController@index')->name('home');
-})
+  
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
@@ -26,7 +26,7 @@ Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')
 ->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
 
-Route::get('/', 'HomeController@index')->name('home');
+});
 
 Auth::routes();
 
