aa778f6cee7047aedf6e53a227465bb4c0861b41
下新原佑哉 <>
2020-01-21 23:46:47 +0900

    ポリシーをミドルウェアを介して使用する

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Routing\Route;
+
 Route::group(['middleware' => 'auth'], function(){
   Route::get('/', 'HomeController@index')->name('home');
 
@@ -19,6 +21,11 @@ Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
 
+Route::group(['middleware' => 'can:view,folder'], function(){
+  Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
+
+
+
 Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
 Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
 
@@ -26,6 +33,7 @@ Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')
 ->name('tasks.edit');
 Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
 
+});
 });
 
 Auth::routes();
