f95c249984ebbfdc487eb645f3f0a6bfc3451f33
Yuya Shimoshimbara <>
2020-02-02 22:28:00 +0900

    一致するid,name,emailを取り出し、テンプレートファイルshowUsersへ渡す

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class UsersController extends Controller
 {
-    //
+    public function showUsers()
+    {
+        $users = new User();
+        $ret = $users->select(['id','name','email'])->where('id', Auth::id())->first();
+        //dd($ret); //デバック用の関数
+        return view('users/showUsers',['item' => $ret]);
+    }
 }
