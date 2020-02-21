4c1309980fee8e2401f1d27554fe6bc81ad2ad94
Yuya Shimoshimbara <>
2020-02-22 00:17:37 +0900

    シェアボタンのロジックを記述

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -55,6 +55,13 @@
                     編集
                   </a>
                 </td>
+                 <td>
+                  <a href="{{ route('tasks.shareurl',[
+                    'task'=>$task->id,
+                    'folder'=>$folder->id
+                    ])}}">シェアする</a>
+                </td>
+
               </tr>
             @endforeach
             </tbody>
