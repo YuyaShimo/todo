d012de35d216b219ef44776ef1a6f2a615228750
Yuya Shimoshimbara <>
2020-02-22 16:41:31 +0900

    タスク共有のためのテンプレートファイル

---
+++ resources/views/tasks/share_task_show.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ToDo App</title>
+  @yield('styles')
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+<div class="panel panel-default">
+  <div class="panel-heading">シェアタスク</div>
+  <div class="panel-body">
+  </div>
+  <table class="table">
+    <thead>
+    <tr>
+      <th>タイトル</th>
+      <th>状態</th>
+      <th>期限</th>
+      <th></th>
+    </tr>
+    </thead>
+    <tbody>
+        <tr>
+          <td>{{ $task->title }}</td>
+          <td>
+            <span class="label {{ $task->status_class }}">{{ $task->status_label }}</span>
+          </td>
+          <td>{{ $task->formatted_due_date }}</td>
+        </tr>
+    </tbody>
+  </table>
+</div>
+</body>
