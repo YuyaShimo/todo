de92cbdeffd6c1c146343c36dca54939775ce578
Yuya Shimoshimbara <>
2020-02-29 00:50:35 +0900

    TaskRepositoryInterfaceの実装クラス

---
+++ app/Repositories/Task/TaskRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories\Task;
+
+use App\Task;
+use Carbon\Carbon;
+
+class TaskRepository implements TaskRepositoryInterface
+{
+  protected $task;
+
+  public function __construct(Task $task)
+  {
+    $this->task = $task;
+  }
+
+  public function updateShareStat($id,$status=1)  //シェアされたのかを判別するカラムである（share_flg）を更新するメソッド
+  {
+      $result = $this->task->where('id',$id)->where('share_flg','!=',1)->update([
+          'share_flg' => $status
+          ]);
+      return !empty($result);
+  }
+
+}
