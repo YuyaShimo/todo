553fd48ca1d4223160684c12930891314a8a966d
Yuya Shimoshimbara <>
2020-03-01 01:04:33 +0900

    インデントが崩れていたので整えた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -30,78 +30,80 @@ class TaskController extends Controller
             'tasks' => $tasks,
         ]);
     }
+
     public function showCreateForm(Folder $folder)
     {
         return view('tasks/create',[
             'folder_id' => $folder->id,
         ]);
     }
+
     public function create(Folder $folder, CreateTask $request)
-{
-
-    $task = new Task();
-    $task->title = $request->title;
-    $task->due_date = $request->due_date;
-
-    $folder->tasks()->save($task);
-
-    return redirect()->route('tasks.index', [
-        'id' => $folder->id,
-    ]);
-}
-
-public function showEditForm(Folder $folder, Task $task)
-{
-    
-    $this->checkRelation($folder, $task);
-
-    return view('tasks/edit',[
-        'task' => $task,
-    ]);
-}
-
-public function edit(Folder $folder, Task $task, EditTask $request)
-{
-    
-    $this->checkRelation($folder, $task);
-
-    $task->title = $request->title;
-    $task->status = $request->status;
-    $task->due_date = $request->due_date;
-    $task->save();
-
-    return redirect()->route('tasks.index',[
-        'id' => $task->folder_id,
-    ]);
-}
-
-private function checkRelation(Folder $folder, Task $task)
-{
-    if($folder->id !== $task->folder_id) {
-        abort(404);
-    }
-}
-
-public function createShareUrl($folder,$task) //シェアボタン押下時にURLを生成
-{
-    $this->task_repo->updateShareStat($task);
-    $share_url = route('tasks.shareshow',[
-        'task' => $task,
-        'folder' => $folder
-        ]);
-        return $share_url;
-} 
-
-public function showShareTask($folder,$task) //シェアしたURLを踏んだ人がログインしていなくてもtaskを閲覧できる
-{
-    $share_task = Task::where('id',$task)->where('share_flg',1)->first();
-    
-    if(empty($share_task))
     {
-        abort(404);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
     }
-    return view('tasks/share_task_show',[
-        'task' => $share_task,
-    ]);
-}
+
+    public function showEditForm(Folder $folder, Task $task)
+    {
+    
+        $this->checkRelation($folder, $task);
+
+        return view('tasks/edit',[
+            'task' => $task,
+        ]);
+    }
+
+    public function edit(Folder $folder, Task $task, EditTask $request)
+    {
+    
+        $this->checkRelation($folder, $task);
+
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        return redirect()->route('tasks.index',[
+            'id' => $task->folder_id,
+        ]);
+    }
+
+    private function checkRelation(Folder $folder, Task $task)
+    {
+        if($folder->id !== $task->folder_id) {
+            abort(404);
+        }
+    }
+
+    public function createShareUrl($folder,$task) //シェアボタン押下時にURLを生成
+    {
+        $this->task_repo->updateShareStat($task);
+        $share_url = route('tasks.shareshow',[
+            'task' => $task,
+            'folder' => $folder
+            ]);
+        return $share_url;
+    } 
+
+    public function showShareTask($folder,$task) //シェアしたURLを踏んだ人がログインしていなくてもtaskを閲覧できる
+    {
+        $share_task = Task::where('id',$task)->where('share_flg',1)->first();
+        
+        if(empty($share_task))
+        {
+            abort(404);
+        }
+        return view('tasks/share_task_show',[
+            'task' => $share_task,
+        ]);
+    }
 }
